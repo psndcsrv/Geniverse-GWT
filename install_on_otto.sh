9464c1125a85b0b06587c302cda70d2dd3f2e7cc
Aaron Unger <aunger@concord.org>
2010-03-08 10:12:01 -0500

    Updated install script. We can't use the war anymore because of the organism image generation.

--- install_on_otto.sh
+++ install_on_otto.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-scp geniverse.war otto.concord.org:/web/aaron/geniverse-jetty/webapps/
+scp -r war/* otto.concord.org:/web/aaron/geniverse-jetty/webapps/geniverse/
+# scp geniverse.war otto.concord.org:/web/aaron/geniverse-jetty/webapps/
+ssh otto.concord.org 'chmod -R g+w /web/aaron/geniverse-jetty/webapps/geniverse/*'
 ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/; ./bin/jetty.sh stop; ./bin/jetty.sh start'
