ea942ece6f21f41d70f99b9fcbc5bfae36e0c471
Aaron Unger <aunger@concord.org>
2010-03-15 15:02:56 -0400

    Add metadata to GOrganism.

--- src/org/concord/geniverse/client/GOrganism.java
+++ src/org/concord/geniverse/client/GOrganism.java
@@ -1,6 +1,8 @@
 package org.concord.geniverse.client;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
@@ -9,6 +11,9 @@ public class GOrganism implements Serializable, IsSerializable {
 	private String name;
 	private int sex;
 	private String alleles;
+	private String imageURL;
+	private ArrayList<String> characteristics;
+	private HashMap<String, Object> metaInfo = new HashMap<String, Object>();
 
 	public void setName(String name) {
 		this.name = name;
@@ -32,5 +37,29 @@ public class GOrganism implements Serializable, IsSerializable {
 
 	public String getAlleles() {
 		return alleles;
+	}
+
+	public void setMetaInfo(HashMap<String, Object> metaInfo) {
+		this.metaInfo = metaInfo;
+	}
+
+	public HashMap<String, Object> getMetaInfo() {
+		return metaInfo;
+	}
+
+	public void setCharacteristics(ArrayList<String> characteristics) {
+		this.characteristics = characteristics;
+	}
+
+	public ArrayList<String> getCharacteristics() {
+		return characteristics;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
+	public String getImageURL() {
+		return imageURL;
 	}
 }
