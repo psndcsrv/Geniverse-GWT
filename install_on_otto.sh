57566bffdc6051c9ad7f23355c0a3d341cb922a9
Aaron Unger <aunger@concord.org>
2010-03-30 13:43:31 -0400

    Change install script for dev.

--- install_on_otto.sh
+++ install_on_otto.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-scp -r war/* otto.concord.org:/web/aaron/geniverse-jetty/webapps/geniverse/
+scp -r war/* otto.concord.org:/web/aaron/geniverse-jetty/webapps/dev/
 # scp geniverse.war otto.concord.org:/web/aaron/geniverse-jetty/webapps/
-ssh otto.concord.org 'chmod -R g+w /web/aaron/geniverse-jetty/webapps/geniverse/*'
-ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/; ./bin/jetty.sh stop; ./bin/jetty.sh start'
+ssh otto.concord.org 'chmod -R g+w /web/aaron/geniverse-jetty/webapps/dev/*'
+ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/'
+ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/ && ./bin/jetty.sh stop'
+ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/ && nohup ./bin/jetty.sh start &'
