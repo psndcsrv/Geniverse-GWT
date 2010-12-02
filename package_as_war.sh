71880d8182711db7c7c133059dcd0eb1259736d5
Sam Fentress <sfentress@dhcp135.concord.org>
2010-12-02 10:25:59 -0500

    Update url of biologica jar to look at latest built by Hudson

--- package_as_war.sh
+++ package_as_war.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 cd war/WEB-INF/lib
 mv biologica-0.1.0-SNAPSHOT.jar ../../../biologica-0.1.0-SNAPSHOT.jar.old
-wget http://jnlp.concord.org/dev/org/concord/biologica/biologica-0.1.0-SNAPSHOT.jar
+wget 'http://source.concord.org/nexus/service/local/artifact/maven/redirect?r=cc-repo-internal-snapshot&g=org.concord&a=biologica&v=0.1.0-SNAPSHOT&e=jar'
 cd -
 
 ant -f buildwar.xml
