c97ca5a1637b3f990176bd75634d3e4f116e726c
Sam Fentress <sfentress@Sam.local>
2010-12-10 13:31:34 -0500

    Update install script to use geniverse user

--- install_on_otto.sh
+++ install_on_otto.sh
@@ -2,11 +2,9 @@
 
 sh package_as_war.sh
 
-ssh otto.concord.org 'rm -rf /web/aaron/geniverse-jetty/webapps/biologica/*'
+ssh geniverse@otto.concord.org 'rm -rf /web/aaron/geniverse-jetty/webapps/biologica/*'
 
-scp -r war/* otto.concord.org:/web/aaron/geniverse-jetty/webapps/biologica/
+scp -r war/* geniverse@otto.concord.org:/web/aaron/geniverse-jetty/webapps/biologica/
 
-ssh otto.concord.org 'sudo sh -c "chown -R aunger.users /web/aaron/geniverse-jetty/webapps/biologica; chmod -R g+rw /web/aaron/geniverse-jetty/webapps/biologica"'
-ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/'
-ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/ && ./bin/jetty.sh stop'
-ssh otto.concord.org 'cd /web/aaron/geniverse-jetty/ && ./bin/jetty.sh start'
+ssh -t geniverse@otto.concord.org 'cd /web/aaron/geniverse-jetty/ && ./bin/jetty.sh stop'
+ssh -t geniverse@otto.concord.org 'cd /web/aaron/geniverse-jetty/ && ./bin/jetty.sh start'
