6f8560b119e608086fc61989c23cc0cf5b39e001
Aaron Unger <aunger@concord.org>
2010-12-16 18:08:06 -0500

    Fix deploy script so that it actually successfully starts jetty back up

--- install_on_otto.sh
+++ install_on_otto.sh
@@ -7,4 +7,4 @@ ssh -t geniverse@otto.concord.org 'cd /web/aaron/geniverse-jetty/ && ./bin/jetty
 ssh geniverse@otto.concord.org 'rm -rf /web/aaron/geniverse-jetty/webapps/biologica/*'
 scp -r war/* geniverse@otto.concord.org:/web/aaron/geniverse-jetty/webapps/biologica/
 
-ssh -t geniverse@otto.concord.org 'cd /web/aaron/geniverse-jetty/ && ./bin/jetty.sh start'
+ssh -t geniverse@otto.concord.org 'cd /web/aaron/geniverse-jetty/ && nohup ./bin/jetty.sh start &'
