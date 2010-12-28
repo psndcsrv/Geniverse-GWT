796dcf9b36d4429f355b009ea2f50c9c63922f0c
Aaron Unger <aunger@concord.org>
2010-12-28 17:41:01 -0500

    Install script that works for multiple deployment environments.

---
+++ install_on_server.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ -x $1 ]; then
+  echo "You must specify which server to install to.\nSupported servers: production, dev"
+  exit 1
+fi
+
+case "$1" in
+  production)
+    export SERVER=seymour.concord.org
+    export SERVER_PATH="/web/production/geniverse.gwt"
+    ;;
+  dev)
+    export SERVER=otto.concord.org
+    export SERVER_PATH="/web/aaron/geniverse-jetty"
+    ;;
+  *)
+    echo "Invalid server!"
+    exit 1
+    ;;
+esac
+
+sh package_as_war.sh
+
+ssh -t geniverse@$SERVER "cd $SERVER_PATH && ./bin/jetty.sh stop"
+
+ssh geniverse@$SERVER "rm -rf $SERVER_PATH/webapps/biologica/*"
+scp -r war/* geniverse@$SERVER:$SERVER_PATH/webapps/biologica/
+
+ssh -t geniverse@$SERVER "cd $SERVER_PATH && ./start.sh && sleep 5"
